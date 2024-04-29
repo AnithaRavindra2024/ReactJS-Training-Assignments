5dfa8ebaef5603e94633492e0ec28878079bbe92
AnithaRavindra2024 <168437376+AnithaRavindra2024@users.noreply.github.com>
2024-04-29 23:19:31 +0530

    Add files via upload

---
+++ ObjectCreation_Access.js
@@ -0,0 +1,17 @@
+//Javascript Object Creation
+
+var studentProfile={
+    fName: 'Anitha',
+    lName :'Ravindra',
+    class:{classname :'10A',subject:'English',teacher:'Indira',email:'indira@test.com'},
+    grades:{Grade1 : 'A',Grade2:'B',Gradec:'C'}
+};
+
+
+let {fName :StudentName ,lName , class:{classname,subject,teacher,email}} =studentProfile ;
+
+console.log(StudentName);
+console.log(lName);
+console.log(classname +' '+subject +' '+teacher +' '+email);
+
+//console.log(`My name is ${fname} and I am ${age} yers old.`);
